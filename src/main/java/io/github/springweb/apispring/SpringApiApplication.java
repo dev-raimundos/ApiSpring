d9afe8afc408ae191f7296085e34330c70fe1145
dev-raimundos <devraimundos@outlook.com>
2025-06-10 18:32:18 -0300

    first commit

---
+++ src/main/java/io/github/springweb/apispring/SpringApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.springweb.apispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiApplication.class, args);
+	}
+
+}
