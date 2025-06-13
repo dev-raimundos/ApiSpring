fffd8f2e1fbd02975984c3453fa441cb083fef5f
dev-raimundos <devraimundos@outlook.com>
2025-06-13 20:31:00 -0300

    FEAT: estruturando post da API

---
+++ src/main/java/io/github/springweb/apispring/controller/ProdutoController.java
@@ -0,0 +1,17 @@
+package io.github.springweb.apispring.controller;
+
+import io.github.springweb.apispring.model.Produto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController
+{
+    @PostMapping
+    public void salvar(@RequestBody Produto produto) {
+        System.out.printf("Produto recebido: " + produto);
+    }
+}
